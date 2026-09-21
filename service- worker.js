56aa5042a56245c1663a5e4c524d11ade0fd8462
stacysleigh194 <stacysleigh194@gmail.com>
2026-09-21 11:15:17 +0300

    Add icons to service worker cache

--- service- worker.js
+++ service- worker.js
@@ -5,7 +5,9 @@ const FILES = [
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png
 ];
 
 self.addEventListener("install", event => {
